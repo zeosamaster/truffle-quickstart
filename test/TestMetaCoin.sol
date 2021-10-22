f752693731ef95896b8c086760ce9cb47a50e3ea
Fábio Matias <fabio_mcm2@hotmail.com>
2021-10-22 11:42:08 +0100

    chore: linting

--- test/TestMetaCoin.sol
+++ test/TestMetaCoin.sol
@@ -5,21 +5,27 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/MetaCoin.sol";
 
 contract TestMetaCoin {
+    function testInitialBalanceUsingDeployedContract() public {
+        MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-  function testInitialBalanceUsingDeployedContract() public {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+        uint256 expected = 10000;
 
-    uint expected = 10000;
+        Assert.equal(
+            meta.getBalance(tx.origin),
+            expected,
+            "Owner should have 10000 MetaCoin initially"
+        );
+    }
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-  }
+    function testInitialBalanceWithNewMetaCoin() public {
+        MetaCoin meta = new MetaCoin();
 
-  function testInitialBalanceWithNewMetaCoin() public {
-    MetaCoin meta = new MetaCoin();
+        uint256 expected = 10000;
 
-    uint expected = 10000;
-
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-  }
-
+        Assert.equal(
+            meta.getBalance(tx.origin),
+            expected,
+            "Owner should have 10000 MetaCoin initially"
+        );
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // Uncommenting the defaults below 
+  // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
@@ -25,17 +25,17 @@ module.exports = {
   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
   // make a backup of your artifacts to a safe location before enabling this feature.
   //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
